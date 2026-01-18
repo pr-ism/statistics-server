11eee92a8f978d6b032fa64e09370f99bea3aee6
apptie <appti@naver.com>
2026-01-18 12:19:17 +0900

    feat: 소셜 로그인 관련 custom exception 전역 예외 처리 로직 추가
    - 로그 추가

--- src/main/java/com/prism/statistics/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prism/statistics/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.prism.statistics.global.exception;
 
+import com.prism.statistics.application.auth.exception.UserMissingException;
 import com.prism.statistics.application.auth.exception.WithdrawnUserLoginException;
 import com.prism.statistics.domain.user.exception.AlreadyWithdrawnUserException;
 import com.prism.statistics.global.exception.dto.response.AuthErrorCode;
@@ -7,6 +8,7 @@ import com.prism.statistics.global.exception.dto.response.DefaultErrorCode;
 import com.prism.statistics.global.exception.dto.response.ErrorCode;
 import com.prism.statistics.global.exception.dto.response.ExceptionResponse;
 import com.prism.statistics.global.exception.dto.response.UserErrorCode;
+import com.prism.statistics.infrastructure.auth.persistence.exception.OrphanedUserIdentityException;
 import com.prism.statistics.presentation.auth.exception.RefreshTokenNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -33,23 +35,45 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ExceptionResponse> handleIllegalStateException() {
+    public ResponseEntity<ExceptionResponse> handleIllegalStateException(IllegalStateException ex) {
+        log.info("IllegalStateException : {}", ex.getMessage());
+
         return createResponseEntity(DefaultErrorCode.INVALID_INPUT_STATE);
     }
 
     @ExceptionHandler(RefreshTokenNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleRefreshTokenNotFoundException() {
+    public ResponseEntity<ExceptionResponse> handleRefreshTokenNotFoundException(RefreshTokenNotFoundException ex) {
+        log.info("RefreshTokenNotFoundException : {}", ex.getMessage());
+
         return createResponseEntity(AuthErrorCode.REFRESH_TOKEN_NOT_FOUND);
     }
 
     @ExceptionHandler(WithdrawnUserLoginException.class)
-    public ResponseEntity<ExceptionResponse> handleWithdrawnUserLoginException() {
+    public ResponseEntity<ExceptionResponse> handleWithdrawnUserLoginException(WithdrawnUserLoginException ex) {
+        log.info("WithdrawnUserLoginException : {}", ex.getMessage());
+
         return createResponseEntity(AuthErrorCode.WITHDRAWN_USER);
     }
 
     @ExceptionHandler(AlreadyWithdrawnUserException.class)
-    public ResponseEntity<ExceptionResponse> handleAlreadyWithdrawnUserException() {
+    public ResponseEntity<ExceptionResponse> handleAlreadyWithdrawnUserException(AlreadyWithdrawnUserException ex) {
+        log.info("AlreadyWithdrawnUserException : {}", ex.getMessage());
+
         return createResponseEntity(UserErrorCode.ALREADY_WITHDRAWN);
+    }
+
+    @ExceptionHandler(OrphanedUserIdentityException.class)
+    public ResponseEntity<ExceptionResponse> handleOrphanedUserIdentityException(OrphanedUserIdentityException ex) {
+        log.info("OrphanedUserIdentityException : {}", ex.getMessage());
+
+        return createResponseEntity(AuthErrorCode.ORPHAN_USER_IDENTITY);
+    }
+
+    @ExceptionHandler(UserMissingException.class)
+    public ResponseEntity<ExceptionResponse> handleUserMissingException(UserMissingException ex) {
+        log.info("UserMissingException : {}", ex.getMessage());
+
+        return createResponseEntity(AuthErrorCode.USER_MISSING);
     }
 
     private ResponseEntity<ExceptionResponse> createResponseEntity(ErrorCode errorCode) {
