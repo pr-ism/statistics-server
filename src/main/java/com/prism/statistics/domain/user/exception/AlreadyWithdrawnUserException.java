e67e39acaf183fe278bc74892c24ad8a15b2578a
apptie <appti@naver.com>
2026-01-19 23:11:33 +0900

    refactor: 탈퇴한 회원이 다시 탈퇴할 때의 custom exception 메시지를 명확하게 변경

--- src/main/java/com/prism/statistics/domain/user/exception/AlreadyWithdrawnUserException.java
+++ src/main/java/com/prism/statistics/domain/user/exception/AlreadyWithdrawnUserException.java
@@ -3,6 +3,6 @@ package com.prism.statistics.domain.user.exception;
 public class AlreadyWithdrawnUserException extends IllegalStateException {
 
     public AlreadyWithdrawnUserException() {
-        super("이미 탈퇴한 회원은 다시 탈퇴할 수 없습니다.");
+        super("이미 탈퇴한 회원입니다.");
     }
 }
