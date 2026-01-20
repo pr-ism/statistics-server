6b8b5fab822ec4a69d74771016f07fe43246f938
apptie <appti@naver.com>
2026-01-20 12:45:38 +0900

    fix: 예외 메시지를 직접 외부로 노출하지 않도록 변경

--- src/main/java/com/prism/statistics/global/security/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/prism/statistics/global/security/handler/OAuth2AuthenticationFailureHandler.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+@Slf4j
 @RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler implements AuthenticationFailureHandler {
 
@@ -24,12 +26,14 @@ public class OAuth2AuthenticationFailureHandler implements AuthenticationFailure
             HttpServletResponse response,
             AuthenticationException exception
     ) throws IOException {
+        log.info("OAuth2 인증 실패", exception);
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
         PrintWriter writer = response.getWriter();
-        AuthExceptionResponse responseBody = new AuthExceptionResponse(exception.getMessage());
+        AuthExceptionResponse responseBody = new AuthExceptionResponse("인증에 실패했습니다.");
 
         writer.println(objectMapper.writeValueAsString(responseBody));
         writer.flush();
--- src/main/java/com/prism/statistics/global/security/handler/OAuth2AuthorizationRequestFailureHandler.java
+++ src/main/java/com/prism/statistics/global/security/handler/OAuth2AuthorizationRequestFailureHandler.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+@Slf4j
 @RequiredArgsConstructor
 public class OAuth2AuthorizationRequestFailureHandler implements AuthenticationFailureHandler {
 
@@ -24,12 +26,14 @@ public class OAuth2AuthorizationRequestFailureHandler implements AuthenticationF
             HttpServletResponse response,
             AuthenticationException exception
     ) throws IOException {
+        log.info("OAuth2 인증 실패", exception);
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setStatus(HttpStatus.BAD_REQUEST.value());
 
         PrintWriter writer = response.getWriter();
-        AuthExceptionResponse body = new AuthExceptionResponse(exception.getMessage());
+        AuthExceptionResponse body = new AuthExceptionResponse("인증 요청 처리 중 오류가 발생했습니다.");
 
         writer.println(objectMapper.writeValueAsString(body));
         writer.flush();
