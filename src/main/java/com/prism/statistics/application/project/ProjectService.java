bbb713045329e038d71d7a280c04b5ddb7840997
gyunnybot <gyunnybot4029@gmail.com>
2026-01-26 20:14:10 +0900

    fix: 팀 컨벤션에 따라 메서드 참조를 스트림으로 변경

--- src/main/java/com/prism/statistics/application/project/ProjectService.java
+++ src/main/java/com/prism/statistics/application/project/ProjectService.java
@@ -3,15 +3,13 @@ package com.prism.statistics.application.project;
 import com.prism.statistics.application.project.dto.request.CreateProjectRequest;
 import com.prism.statistics.application.project.dto.response.CreateProjectResponse;
 import com.prism.statistics.application.project.dto.response.ProjectListResponse;
-import com.prism.statistics.application.project.dto.response.ProjectResponse;
 import com.prism.statistics.domain.project.Project;
 import com.prism.statistics.domain.project.ProjectApiKeyGenerator;
 import com.prism.statistics.domain.project.repository.ProjectRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +28,10 @@ public class ProjectService {
     }
 
     @Transactional(readOnly = true)
-    public ProjectListResponse findByUserId(final Long userId) {
-        List<ProjectResponse> projects = projectRepository.findByUserId(userId).stream()
-                                                          .map(ProjectResponse::from)
-                                                          .toList();
+    public ProjectListResponse findByUserId(Long userId) {
+        List<ProjectListResponse.ProjectResponse> projects = projectRepository.findByUserId(userId).stream()
+                .map(project -> ProjectListResponse.ProjectResponse.from(project))
+                .toList();
 
         return new ProjectListResponse(projects);
     }
