129224a4c9d6515c94060bc3e91c6d1774a29626
apptie <appti@naver.com>
2026-01-28 00:53:15 +0900

    refactor: 사용하지 않는 access token 쿠키 이름 상수 삭제

--- src/main/java/com/prism/statistics/global/security/constants/TokenCookieName.java
+++ src/main/java/com/prism/statistics/global/security/constants/TokenCookieName.java
@@ -2,7 +2,6 @@ package com.prism.statistics.global.security.constants;
 
 public final class TokenCookieName {
 
-    public static final String ACCESS_TOKEN = "accessToken";
     public static final String REFRESH_TOKEN = "refreshToken";
 
     private TokenCookieName() {
