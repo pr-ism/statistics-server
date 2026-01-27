1a41e80a27c80e128bbf7208e67104702fbf4ee0
HyNS00 <hsk7953@gmail.com>
2026-01-27 21:59:21 +0900

    test: FileChangeTypeTest 개선
    - @CsvSource를 @MethodSource로 변경하여 enum 네이밍 변경에 안전
    - 로직 수행과 검증을 // when & then으로 통일

--- src/test/java/com/prism/statistics/domain/pullrequest/enums/FileChangeTypeTest.java
+++ src/test/java/com/prism/statistics/domain/pullrequest/enums/FileChangeTypeTest.java
@@ -3,59 +3,65 @@ package com.prism.statistics.domain.pullrequest.enums;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FileChangeTypeTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "modified, MODIFIED",
-            "added, ADDED",
-            "removed, REMOVED",
-            "renamed, RENAMED"
-    })
+    @MethodSource("소문자_상태_테스트_데이터")
     void 소문자_상태로_FileChangeType을_생성한다(String status, FileChangeType expected) {
-        // when
-        FileChangeType actual = FileChangeType.fromGitHubStatus(status);
+        // when & then
+        assertThat(FileChangeType.fromGitHubStatus(status)).isEqualTo(expected);
+    }
 
-        // then
-        assertThat(actual).isEqualTo(expected);
+    static Stream<Arguments> 소문자_상태_테스트_데이터() {
+        return Stream.of(
+                Arguments.of("modified", FileChangeType.MODIFIED),
+                Arguments.of("added", FileChangeType.ADDED),
+                Arguments.of("removed", FileChangeType.REMOVED),
+                Arguments.of("renamed", FileChangeType.RENAMED)
+        );
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "MODIFIED, MODIFIED",
-            "ADDED, ADDED",
-            "REMOVED, REMOVED",
-            "RENAMED, RENAMED"
-    })
+    @MethodSource("대문자_상태_테스트_데이터")
     void 대문자_상태로_FileChangeType을_생성한다(String status, FileChangeType expected) {
-        // when
-        FileChangeType actual = FileChangeType.fromGitHubStatus(status);
+        // when & then
+        assertThat(FileChangeType.fromGitHubStatus(status)).isEqualTo(expected);
+    }
 
-        // then
-        assertThat(actual).isEqualTo(expected);
+    static Stream<Arguments> 대문자_상태_테스트_데이터() {
+        return Stream.of(
+                Arguments.of("MODIFIED", FileChangeType.MODIFIED),
+                Arguments.of("ADDED", FileChangeType.ADDED),
+                Arguments.of("REMOVED", FileChangeType.REMOVED),
+                Arguments.of("RENAMED", FileChangeType.RENAMED)
+        );
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "Modified, MODIFIED",
-            "Added, ADDED",
-            "Removed, REMOVED",
-            "Renamed, RENAMED"
-    })
+    @MethodSource("대소문자_혼합_상태_테스트_데이터")
     void 대소문자_혼합_상태로_FileChangeType을_생성한다(String status, FileChangeType expected) {
-        // when
-        FileChangeType actual = FileChangeType.fromGitHubStatus(status);
+        // when & then
+        assertThat(FileChangeType.fromGitHubStatus(status)).isEqualTo(expected);
+    }
 
-        // then
-        assertThat(actual).isEqualTo(expected);
+    static Stream<Arguments> 대소문자_혼합_상태_테스트_데이터() {
+        return Stream.of(
+                Arguments.of("Modified", FileChangeType.MODIFIED),
+                Arguments.of("Added", FileChangeType.ADDED),
+                Arguments.of("Removed", FileChangeType.REMOVED),
+                Arguments.of("Renamed", FileChangeType.RENAMED)
+        );
     }
 
     @Test
