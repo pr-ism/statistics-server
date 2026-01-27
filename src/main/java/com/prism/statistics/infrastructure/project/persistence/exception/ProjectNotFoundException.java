37ae58f3c8a9ba074a8a1079e33eccb5a1ff508e
HyNS00 <hsk7953@gmail.com>
2026-01-27 16:04:44 +0900

    refactor: ProjectNotFoundException 생성자를 기본 생성자로 변경

--- src/main/java/com/prism/statistics/infrastructure/project/persistence/exception/ProjectNotFoundException.java
+++ src/main/java/com/prism/statistics/infrastructure/project/persistence/exception/ProjectNotFoundException.java
@@ -2,7 +2,7 @@ package com.prism.statistics.infrastructure.project.persistence.exception;
 
 public class ProjectNotFoundException extends RuntimeException {
 
-    public ProjectNotFoundException(String name) {
-        super("프로젝트를 찾을 수 없습니다: " + name);
+    public ProjectNotFoundException() {
+        super("유효하지 않은 API Key입니다.");
     }
 }
