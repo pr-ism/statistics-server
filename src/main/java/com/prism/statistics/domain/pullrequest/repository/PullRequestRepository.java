1251f2d66bf486e2e3eefc6b88eb462d22ca07bd
HyNS00 <hsk7953@gmail.com>
2026-01-28 20:14:49 +0900

    feat: PullRequest 조회 기능 추가

--- src/main/java/com/prism/statistics/domain/pullrequest/repository/PullRequestRepository.java
+++ src/main/java/com/prism/statistics/domain/pullrequest/repository/PullRequestRepository.java
@@ -2,7 +2,11 @@ package com.prism.statistics.domain.pullrequest.repository;
 
 import com.prism.statistics.domain.pullrequest.PullRequest;
 
+import java.util.Optional;
+
 public interface PullRequestRepository {
 
     PullRequest save(PullRequest pullRequest);
+
+    Optional<PullRequest> findPullRequest(Long projectId, int prNumber);
 }
