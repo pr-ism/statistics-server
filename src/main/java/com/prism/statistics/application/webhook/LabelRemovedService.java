e29b704a66111a496684c6fd7f701092855cb16d
HyNS00 <hsk7953@gmail.com>
2026-01-28 21:01:29 +0900

    feat: LabelRemovedService 추가

---
+++ src/main/java/com/prism/statistics/application/webhook/LabelRemovedService.java
@@ -0,0 +1,62 @@
+package com.prism.statistics.application.webhook;
+
+import com.prism.statistics.application.webhook.dto.request.LabelRemovedRequest;
+import com.prism.statistics.domain.label.PrLabelHistory;
+import com.prism.statistics.domain.label.enums.LabelAction;
+import com.prism.statistics.domain.label.repository.PrLabelHistoryRepository;
+import com.prism.statistics.domain.label.repository.PrLabelRepository;
+import com.prism.statistics.domain.project.repository.ProjectRepository;
+import com.prism.statistics.domain.pullrequest.PullRequest;
+import com.prism.statistics.domain.pullrequest.repository.PullRequestRepository;
+import com.prism.statistics.infrastructure.project.persistence.exception.ProjectNotFoundException;
+import com.prism.statistics.infrastructure.pullrequest.persistence.exception.PullRequestNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class LabelRemovedService {
+
+    private final Clock clock;
+    private final ProjectRepository projectRepository;
+    private final PullRequestRepository pullRequestRepository;
+    private final PrLabelRepository prLabelRepository;
+    private final PrLabelHistoryRepository prLabelHistoryRepository;
+
+    @Transactional
+    public void removeLabel(String apiKey, LabelRemovedRequest request) {
+        Long projectId = projectRepository.findIdByApiKey(apiKey)
+                .orElseThrow(() -> new ProjectNotFoundException());
+
+        PullRequest pullRequest = pullRequestRepository.findPullRequest(projectId, request.prNumber())
+                .orElseThrow(() -> new PullRequestNotFoundException());
+
+        String labelName = request.label().name();
+        Long pullRequestId = pullRequest.getId();
+
+        if (!prLabelRepository.exists(pullRequestId, labelName)) {
+            return;
+        }
+
+        LocalDateTime unlabeledAt = toLocalDateTime(request.unlabeledAt());
+
+        prLabelRepository.deleteLabel(pullRequestId, labelName);
+
+        PrLabelHistory prLabelHistory = PrLabelHistory.create(
+                pullRequest.getId(),
+                labelName,
+                LabelAction.REMOVED,
+                unlabeledAt
+        );
+        prLabelHistoryRepository.save(prLabelHistory);
+    }
+
+    private LocalDateTime toLocalDateTime(Instant instant) {
+        return LocalDateTime.ofInstant(instant, clock.getZone());
+    }
+}
