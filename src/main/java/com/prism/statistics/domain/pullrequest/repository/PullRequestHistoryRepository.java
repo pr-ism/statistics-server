2b516b486ac5fd437ad981db5a5cc90658d0f774
HyNS00 <hsk7953@gmail.com>
2026-01-30 22:11:03 +0900

    refactor: PullRequestHistory를 PullRequestContentHistory 로 변경

--- src/main/java/com/prism/statistics/domain/pullrequest/repository/PullRequestHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prism.statistics.domain.pullrequest.repository;
-
-import com.prism.statistics.domain.pullrequest.PullRequestHistory;
-
-public interface PullRequestHistoryRepository {
-
-    PullRequestHistory save(PullRequestHistory pullRequestHistory);
-}
