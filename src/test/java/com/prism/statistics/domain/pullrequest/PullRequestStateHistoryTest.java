16ddfd6cd28c9b622484b513a7d17e14667f55b9
HyNS00 <hsk7953@gmail.com>
2026-01-30 22:03:55 +0900

    refactor: 테스트 네이밍을 확실하게 변경(PR_ID -> Pull_Request_ID)

--- src/test/java/com/prism/statistics/domain/pullrequest/PullRequestStateHistoryTest.java
+++ src/test/java/com/prism/statistics/domain/pullrequest/PullRequestStateHistoryTest.java
@@ -64,7 +64,7 @@ class PullRequestStateHistoryTest {
     }
 
     @Test
-    void PR_ID가_null이면_예외가_발생한다() {
+    void Pull_Request_ID가_null이면_예외가_발생한다() {
         // when & then
         assertThatThrownBy(() -> PullRequestStateHistory.create(null, PrState.OPEN, PrState.MERGED, CHANGED_AT))
                 .isInstanceOf(IllegalArgumentException.class)
