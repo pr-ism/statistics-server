a42c72308eb91b6d6ead844438acecf50fc3416e
gyunnybot <gyunnybot4029@gmail.com>
2026-01-31 15:54:03 +0900

    feat: 라벨별 PR 통계 service 추가

---
+++ src/main/java/com/prism/statistics/application/metric/LabelStatisticsQueryService.java
@@ -0,0 +1,37 @@
+package com.prism.statistics.application.metric;
+
+import com.prism.statistics.application.metric.dto.response.LabelStatisticsResponse;
+import com.prism.statistics.application.metric.dto.response.LabelStatisticsResponse.LabelStatistics;
+import com.prism.statistics.domain.metric.repository.LabelStatisticsRepository;
+import com.prism.statistics.domain.project.repository.ProjectRepository;
+import com.prism.statistics.infrastructure.project.persistence.exception.ProjectNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class LabelStatisticsQueryService {
+
+    private final LabelStatisticsRepository labelStatisticsRepository;
+    private final ProjectRepository projectRepository;
+
+    @Transactional(readOnly = true)
+    public LabelStatisticsResponse findLabelStatistics(Long userId, Long projectId) {
+        validateProjectOwnership(projectId, userId);
+
+        List<LabelStatistics> labelStatistics = labelStatisticsRepository
+                .findLabelStatisticsByProjectId(projectId).stream()
+                .map(labelStat -> LabelStatistics.from(labelStat))
+                .toList();
+
+        return new LabelStatisticsResponse(labelStatistics);
+    }
+
+    private void validateProjectOwnership(Long projectId, Long userId) {
+        if (!projectRepository.existsByIdAndUserId(projectId, userId)) {
+            throw new ProjectNotFoundException();
+        }
+    }
+}
