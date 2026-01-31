6656609ee51494af4c33bf730f7f311c1f1f5ba7
gyunnybot <gyunnybot4029@gmail.com>
2026-01-31 14:23:26 +0900

    feat: 작성자별 통계 repository 추가

---
+++ src/main/java/com/prism/statistics/domain/metric/repository/AuthorStatisticsRepository.java
@@ -0,0 +1,9 @@
+package com.prism.statistics.domain.metric.repository;
+
+import com.prism.statistics.domain.metric.dto.AuthorStatisticsDto;
+import java.util.List;
+
+public interface AuthorStatisticsRepository {
+
+    List<AuthorStatisticsDto> findAuthorStatisticsByProjectId(Long projectId);
+}
---
+++ src/main/java/com/prism/statistics/infrastructure/metric/persistence/AuthorStatisticsRepositoryAdapter.java
@@ -0,0 +1,42 @@
+package com.prism.statistics.infrastructure.metric.persistence;
+
+import static com.prism.statistics.domain.pullrequest.QPullRequest.pullRequest;
+
+import com.prism.statistics.domain.metric.dto.AuthorStatisticsDto;
+import com.prism.statistics.domain.metric.repository.AuthorStatisticsRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+public class AuthorStatisticsRepositoryAdapter implements AuthorStatisticsRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<AuthorStatisticsDto> findAuthorStatisticsByProjectId(Long projectId) {
+        return queryFactory
+                .select(
+                        Projections.constructor(
+                                AuthorStatisticsDto.class,
+                                pullRequest.authorGithubId,
+                                pullRequest.count(),
+                                pullRequest.changeStats.additionCount.sumLong().coalesce(0L),
+                                pullRequest.changeStats.deletionCount.sumLong().coalesce(0L),
+                                pullRequest.changeStats.additionCount.avg().coalesce(0.0D),
+                                pullRequest.changeStats.deletionCount.avg().coalesce(0.0D),
+                                pullRequest.commitCount.avg().coalesce(0.0D),
+                                pullRequest.changeStats.changedFileCount.avg().coalesce(0.0D)
+                        )
+                )
+                .from(pullRequest)
+                .where(pullRequest.projectId.eq(projectId))
+                .groupBy(pullRequest.authorGithubId)
+                .fetch();
+    }
+}
