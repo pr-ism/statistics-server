e922b09b7e574ab26a84d57cdfe5c73dacaf3765
HyNS00 <hsk7953@gmail.com>
2026-02-03 17:18:50 +0900

    feat: ReviewCommentCreatedService 추가

---
+++ src/main/java/com/prism/statistics/application/webhook/ReviewCommentCreatedService.java
@@ -0,0 +1,59 @@
+package com.prism.statistics.application.webhook;
+
+import com.prism.statistics.application.webhook.dto.request.ReviewCommentCreatedRequest;
+import com.prism.statistics.domain.project.repository.ProjectRepository;
+import com.prism.statistics.domain.reviewcomment.ReviewComment;
+import com.prism.statistics.domain.reviewcomment.repository.ReviewCommentRepository;
+import com.prism.statistics.infrastructure.project.persistence.exception.InvalidApiKeyException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewCommentCreatedService {
+
+    private final Clock clock;
+    private final ProjectRepository projectRepository;
+    private final ReviewCommentRepository reviewCommentRepository;
+
+    public void createReviewComment(String apiKey, ReviewCommentCreatedRequest request) {
+        validateApiKey(apiKey);
+
+        ReviewComment reviewComment = createReviewComment(request);
+        reviewCommentRepository.saveOrFind(reviewComment);
+    }
+
+    private void validateApiKey(String apiKey) {
+        projectRepository.findIdByApiKey(apiKey)
+                .orElseThrow(() -> new InvalidApiKeyException());
+    }
+
+    private ReviewComment createReviewComment(ReviewCommentCreatedRequest request) {
+        LocalDateTime createdAt = toLocalDateTime(request.createdAt());
+        LocalDateTime updatedAt = toLocalDateTime(request.updatedAt());
+
+        return ReviewComment.create(
+                request.githubCommentId(),
+                request.githubReviewId(),
+                request.body(),
+                request.path(),
+                request.startLine(),
+                request.line(),
+                request.side(),
+                request.commitSha(),
+                request.inReplyToId(),
+                request.author().login(),
+                request.author().id(),
+                createdAt,
+                updatedAt
+        );
+    }
+
+    private LocalDateTime toLocalDateTime(Instant instant) {
+        return LocalDateTime.ofInstant(instant, clock.getZone());
+    }
+}
