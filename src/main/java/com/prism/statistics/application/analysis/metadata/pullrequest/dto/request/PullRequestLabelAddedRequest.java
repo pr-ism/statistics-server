5b721310394cc8343c21d0aee706f5bac228da4e
HyNS00 <hsk7953@gmail.com>
2026-02-09 14:46:31 +0900

    feat: github에서 제공하는 pullRequestId 추가 및 추적용 headcommit 추가

--- src/main/java/com/prism/statistics/application/analysis/metadata/pullrequest/dto/request/PullRequestLabelAddedRequest.java
+++ src/main/java/com/prism/statistics/application/analysis/metadata/pullrequest/dto/request/PullRequestLabelAddedRequest.java
@@ -3,7 +3,9 @@ package com.prism.statistics.application.analysis.metadata.pullrequest.dto.reque
 import java.time.Instant;
 
 public record PullRequestLabelAddedRequest(
+        Long githubPullRequestId,
         int pullRequestNumber,
+        String headCommitSha,
         LabelData label,
         Instant labeledAt
 ) {
