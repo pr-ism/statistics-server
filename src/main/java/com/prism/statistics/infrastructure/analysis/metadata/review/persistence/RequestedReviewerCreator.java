882d8d40f7a20cb1fe7a842eb2155a62d6c43d8b
HyNS00 <hsk7953@gmail.com>
2026-02-09 20:15:47 +0900

    feat: RequestedReviewer의 saveOrFind구현

---
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/RequestedReviewerCreator.java
@@ -0,0 +1,22 @@
+package com.prism.statistics.infrastructure.analysis.metadata.review.persistence;
+
+import com.prism.statistics.domain.analysis.metadata.review.RequestedReviewer;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+public class RequestedReviewerCreator {
+
+    private final JpaRequestedReviewerRepository jpaRequestedReviewerRepository;
+    private final EntityManager entityManager;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveNew(RequestedReviewer requestedReviewer) {
+        jpaRequestedReviewerRepository.save(requestedReviewer);
+        entityManager.flush();
+    }
+}
--- src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/RequestedReviewerRepositoryAdapter.java
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/RequestedReviewerRepositoryAdapter.java
@@ -4,22 +4,38 @@ import static com.prism.statistics.domain.analysis.metadata.review.QRequestedRev
 
 import com.prism.statistics.domain.analysis.metadata.review.RequestedReviewer;
 import com.prism.statistics.domain.analysis.metadata.review.repository.RequestedReviewerRepository;
+import com.prism.statistics.infrastructure.common.MysqlDuplicateKeyDetector;
+import com.prism.statistics.infrastructure.analysis.metadata.review.persistence.exception.RequestedReviewerNotFoundException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class RequestedReviewerRepositoryAdapter implements RequestedReviewerRepository {
 
+    private final RequestedReviewerCreator requestedReviewerCreator;
     private final JpaRequestedReviewerRepository jpaRequestedReviewerRepository;
     private final JPAQueryFactory queryFactory;
+    private final MysqlDuplicateKeyDetector duplicateKeyDetector;
 
     @Override
-    @Transactional
-    public RequestedReviewer save(RequestedReviewer requestedReviewer) {
-        return jpaRequestedReviewerRepository.save(requestedReviewer);
+    public RequestedReviewer saveOrFind(RequestedReviewer reviewer) {
+        try {
+            requestedReviewerCreator.saveNew(reviewer);
+            return reviewer;
+        } catch (DataIntegrityViolationException ex) {
+            if (duplicateKeyDetector.isDuplicateKey(ex)) {
+                return jpaRequestedReviewerRepository.findByGithubPullRequestIdAndReviewerUserId(
+                        reviewer.getGithubPullRequestId(), reviewer.getReviewer().getUserId()
+                ).orElseThrow(() -> new RequestedReviewerNotFoundException());
+            }
+            throw ex;
+        }
     }
 
     @Override
@@ -43,6 +59,28 @@ public class RequestedReviewerRepositoryAdapter implements RequestedReviewerRepo
                 .where(
                         requestedReviewer.pullRequestId.eq(pullRequestId),
                         requestedReviewer.githubUid.eq(githubUid)
+                )
+                .execute();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<RequestedReviewer> findByGithubPullRequestIdAndUserId(
+            Long githubPullRequestId, Long userId
+    ) {
+        return jpaRequestedReviewerRepository.findByGithubPullRequestIdAndReviewerUserId(
+                githubPullRequestId, userId
+        );
+    }
+
+    @Override
+    @Transactional
+    public long deleteByGithubId(Long githubPullRequestId, Long userId) {
+        return queryFactory
+                .delete(requestedReviewer)
+                .where(
+                        requestedReviewer.githubPullRequestId.eq(githubPullRequestId),
+                        requestedReviewer.reviewer.userId.eq(userId)
                 )
                 .execute();
     }
