50141e396a782813e343237e22c52558d2fcadf1
HyNS00 <hsk7953@gmail.com>
2026-02-09 21:30:55 +0900

    refactor: builder 사용

--- src/main/java/com/prism/statistics/domain/analysis/metadata/review/RequestedReviewer.java
+++ src/main/java/com/prism/statistics/domain/analysis/metadata/review/RequestedReviewer.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -35,46 +36,62 @@ public class RequestedReviewer extends CreatedAtEntity {
             GithubUser reviewer,
             LocalDateTime requestedAt
     ) {
-        validateGithubPullRequestId(githubPullRequestId);
-        validateHeadCommitSha(headCommitSha);
-        validateReviewer(reviewer);
-        validateRequestedAt(requestedAt);
-        return new RequestedReviewer(githubPullRequestId, headCommitSha, reviewer, requestedAt);
+        return RequestedReviewer.builder()
+                .githubPullRequestId(githubPullRequestId)
+                .headCommitSha(headCommitSha)
+                .reviewer(reviewer)
+                .requestedAt(requestedAt)
+                .build();
     }
 
-    private static void validateGithubPullRequestId(Long githubPullRequestId) {
-        if (githubPullRequestId == null) {
-            throw new IllegalArgumentException("Github PullRequest ID는 필수입니다.");
-        }
-    }
-
-    private static void validateHeadCommitSha(String headCommitSha) {
-        if (headCommitSha == null || headCommitSha.isBlank()) {
-            throw new IllegalArgumentException("Head Commit SHA는 필수입니다.");
-        }
-    }
-
-    private static void validateReviewer(GithubUser reviewer) {
-        if (reviewer == null) {
-            throw new IllegalArgumentException("리뷰어는 필수입니다.");
-        }
-    }
-
-    private static void validateRequestedAt(LocalDateTime requestedAt) {
-        if (requestedAt == null) {
-            throw new IllegalArgumentException("리뷰어 요청 시각은 필수입니다.");
-        }
-    }
-
+    @Builder
     private RequestedReviewer(
             Long githubPullRequestId,
             String headCommitSha,
             GithubUser reviewer,
             LocalDateTime requestedAt
     ) {
+        validateFields(githubPullRequestId, headCommitSha, reviewer, requestedAt);
+
         this.githubPullRequestId = githubPullRequestId;
         this.headCommitSha = headCommitSha;
         this.reviewer = reviewer;
         this.requestedAt = requestedAt;
+    }
+
+    private void validateFields(
+            Long githubPullRequestId,
+            String headCommitSha,
+            GithubUser reviewer,
+            LocalDateTime requestedAt
+    ) {
+        validateGithubPullRequestId(githubPullRequestId);
+        validateHeadCommitSha(headCommitSha);
+        validateReviewer(reviewer);
+        validateRequestedAt(requestedAt);
+    }
+
+    private void validateGithubPullRequestId(Long githubPullRequestId) {
+        if (githubPullRequestId == null) {
+            throw new IllegalArgumentException("Github PullRequest ID는 필수입니다.");
+        }
+    }
+
+    private void validateHeadCommitSha(String headCommitSha) {
+        if (headCommitSha == null || headCommitSha.isBlank()) {
+            throw new IllegalArgumentException("Head Commit SHA는 필수입니다.");
+        }
+    }
+
+    private void validateReviewer(GithubUser reviewer) {
+        if (reviewer == null) {
+            throw new IllegalArgumentException("리뷰어는 필수입니다.");
+        }
+    }
+
+    private void validateRequestedAt(LocalDateTime requestedAt) {
+        if (requestedAt == null) {
+            throw new IllegalArgumentException("리뷰어 요청 시각은 필수입니다.");
+        }
     }
 }
