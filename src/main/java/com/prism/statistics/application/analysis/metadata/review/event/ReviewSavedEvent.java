480efe105a2c20b936ed35f75a19bf27faa512ae
HyNS00 <hsk7953@gmail.com>
2026-02-10 23:59:44 +0900

    feat: Review에 대한 event record 추가

---
+++ src/main/java/com/prism/statistics/application/analysis/metadata/review/event/ReviewSavedEvent.java
@@ -0,0 +1,6 @@
+package com.prism.statistics.application.analysis.metadata.review.event;
+
+import com.prism.statistics.domain.analysis.metadata.review.Review;
+
+public record ReviewSavedEvent(Review review) {
+}
