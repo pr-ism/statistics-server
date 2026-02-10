6c252a385d59f4f84b79de7f5f8fa2b63b9d41c4
HyNS00 <hsk7953@gmail.com>
2026-02-10 23:16:16 +0900

    feat: RequestedReviewer 저장 시 발행할 이벤트 레코드 추가

---
+++ src/main/java/com/prism/statistics/application/analysis/metadata/review/event/RequestedReviewerSavedEvent.java
@@ -0,0 +1,6 @@
+package com.prism.statistics.application.analysis.metadata.review.event;
+
+import com.prism.statistics.domain.analysis.metadata.review.RequestedReviewer;
+
+public record RequestedReviewerSavedEvent(RequestedReviewer requestedReviewer) {
+}
