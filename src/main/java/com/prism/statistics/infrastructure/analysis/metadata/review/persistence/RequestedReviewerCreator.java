fd507609d938078b84e4589738d25d18888247af
HyNS00 <hsk7953@gmail.com>
2026-02-10 23:26:53 +0900

    feat: RequestedReviewer event 발행 추가

--- src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/RequestedReviewerCreator.java
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/RequestedReviewerCreator.java
@@ -1,8 +1,10 @@
 package com.prism.statistics.infrastructure.analysis.metadata.review.persistence;
 
+import com.prism.statistics.application.analysis.metadata.review.event.RequestedReviewerSavedEvent;
 import com.prism.statistics.domain.analysis.metadata.review.RequestedReviewer;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,13 @@ public class RequestedReviewerCreator {
 
     private final JpaRequestedReviewerRepository jpaRequestedReviewerRepository;
     private final EntityManager entityManager;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public RequestedReviewer saveNew(RequestedReviewer requestedReviewer) {
         RequestedReviewer saved = jpaRequestedReviewerRepository.save(requestedReviewer);
         entityManager.flush();
+        eventPublisher.publishEvent(new RequestedReviewerSavedEvent(saved));
         return saved;
     }
 }
