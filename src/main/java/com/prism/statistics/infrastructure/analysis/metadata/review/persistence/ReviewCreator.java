3cb132525544551932a0b8eac58af716efe3d364
HyNS00 <hsk7953@gmail.com>
2026-02-11 01:26:41 +0900

    feat: ReviewCreator에 이벤트 발행 추가

--- src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/ReviewCreator.java
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/ReviewCreator.java
@@ -1,8 +1,10 @@
 package com.prism.statistics.infrastructure.analysis.metadata.review.persistence;
 
+import com.prism.statistics.application.analysis.metadata.review.event.ReviewSavedEvent;
 import com.prism.statistics.domain.analysis.metadata.review.Review;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,13 @@ public class ReviewCreator {
 
     private final JpaReviewRepository jpaReviewRepository;
     private final EntityManager entityManager;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Review saveNew(Review review) {
         Review saved = jpaReviewRepository.save(review);
         entityManager.flush();
+        eventPublisher.publishEvent(new ReviewSavedEvent(saved));
         return saved;
     }
 }
