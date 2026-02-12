5cd549e4f7a2932c46caf2fe832319e072e9fe78
HyNS00 <hsk7953@gmail.com>
2026-02-12 16:16:29 +0900

    feat: @Async 비동기 메서드의 미처리 예외 로깅 추가

--- src/main/java/com/prism/statistics/global/config/AsyncConfig.java
+++ src/main/java/com/prism/statistics/global/config/AsyncConfig.java
@@ -3,19 +3,29 @@ package com.prism.statistics.global.config;
 import com.prism.statistics.global.config.properties.BackfillAsyncProperties;
 import com.prism.statistics.global.config.properties.PullRequestOpenedDerivedMetricsAsyncProperties;
 import java.util.concurrent.Executor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Slf4j
 @EnableAsync
 @Configuration
 @EnableConfigurationProperties({
         PullRequestOpenedDerivedMetricsAsyncProperties.class,
         BackfillAsyncProperties.class
 })
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) ->
+                log.error("Async Exception in {} : {}", method.getName(), ex.getMessage(), ex);
+    }
 
     @Bean(name = "asyncTaskExecutor")
     public Executor asyncTaskExecutor(PullRequestOpenedDerivedMetricsAsyncProperties properties) {
