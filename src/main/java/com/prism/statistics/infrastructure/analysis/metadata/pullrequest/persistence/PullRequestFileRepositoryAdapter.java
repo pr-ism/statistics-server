b1cf4ea0e9a6aed6befffdf3702512a0804dba06
HyNS00 <hsk7953@gmail.com>
2026-02-18 20:21:38 +0900

    feat: PullRequestFileRepository에 deleteAllByPullRequestId 추가

--- src/main/java/com/prism/statistics/infrastructure/analysis/metadata/pullrequest/persistence/PullRequestFileRepositoryAdapter.java
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/pullrequest/persistence/PullRequestFileRepositoryAdapter.java
@@ -1,7 +1,10 @@
 package com.prism.statistics.infrastructure.analysis.metadata.pullrequest.persistence;
 
+import static com.prism.statistics.domain.analysis.metadata.pullrequest.QPullRequestFile.pullRequestFile;
+
 import com.prism.statistics.domain.analysis.metadata.pullrequest.PullRequestFile;
 import com.prism.statistics.domain.analysis.metadata.pullrequest.repository.PullRequestFileRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,7 @@ import java.util.List;
 public class PullRequestFileRepositoryAdapter implements PullRequestFileRepository {
 
     private final JpaPullRequestFileRepository jpaPullRequestFileRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     @Transactional
@@ -24,5 +28,14 @@ public class PullRequestFileRepositoryAdapter implements PullRequestFileReposito
     @Transactional
     public List<PullRequestFile> saveAll(List<PullRequestFile> pullRequestFiles) {
         return jpaPullRequestFileRepository.saveAll(pullRequestFiles);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAllByPullRequestId(Long pullRequestId) {
+        queryFactory
+                .delete(pullRequestFile)
+                .where(pullRequestFile.pullRequestId.eq(pullRequestId))
+                .execute();
     }
 }
