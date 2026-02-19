8a0211b8bff01d0e1602c5f470f41a5d12ba9571
gyunnybot <gyunnybot4029@gmail.com>
2026-02-19 22:35:35 +0900

    refactor: PR 크기 가중치, 임계값, 코어 타임 생성 관련 저장 로직 추가

--- src/main/java/com/prism/statistics/application/project/ProjectService.java
+++ src/main/java/com/prism/statistics/application/project/ProjectService.java
@@ -6,6 +6,12 @@ import com.prism.statistics.application.project.dto.response.ProjectListResponse
 import com.prism.statistics.domain.project.Project;
 import com.prism.statistics.domain.project.ProjectApiKeyGenerator;
 import com.prism.statistics.domain.project.repository.ProjectRepository;
+import com.prism.statistics.domain.project.setting.ProjectCoreTimeSetting;
+import com.prism.statistics.domain.project.setting.ProjectSizeGradeThresholdSetting;
+import com.prism.statistics.domain.project.setting.ProjectSizeWeightSetting;
+import com.prism.statistics.domain.project.setting.repository.ProjectCoreTimeSettingRepository;
+import com.prism.statistics.domain.project.setting.repository.ProjectSizeGradeThresholdSettingRepository;
+import com.prism.statistics.domain.project.setting.repository.ProjectSizeWeightSettingRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,12 +23,19 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectApiKeyGenerator projectApiKeyGenerator;
+    private final ProjectCoreTimeSettingRepository projectCoreTimeSettingRepository;
+    private final ProjectSizeWeightSettingRepository projectSizeWeightSettingRepository;
+    private final ProjectSizeGradeThresholdSettingRepository projectSizeGradeThresholdSettingRepository;
 
     @Transactional
     public CreateProjectResponse create(Long userId, CreateProjectRequest request) {
         String apiKey = projectApiKeyGenerator.generate();
         Project project = Project.create(request.name(), apiKey, userId);
         Project savedProject = projectRepository.save(project);
+
+        projectCoreTimeSettingRepository.save(ProjectCoreTimeSetting.createDefault(savedProject.getId()));
+        projectSizeWeightSettingRepository.save(ProjectSizeWeightSetting.createDefault(savedProject.getId()));
+        projectSizeGradeThresholdSettingRepository.save(ProjectSizeGradeThresholdSetting.createDefault(savedProject.getId()));
 
         return CreateProjectResponse.from(savedProject);
     }
