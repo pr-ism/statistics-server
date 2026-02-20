94eca7f67466204bc4efaeaa9b4b6cfd4bbd1726
gyunnybot <gyunnybot4029@gmail.com>
2026-02-20 21:39:20 +0900

    feat: findAllByPullRequestId() 메서드 추가

--- src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/JpaReviewRepository.java
+++ src/main/java/com/prism/statistics/infrastructure/analysis/metadata/review/persistence/JpaReviewRepository.java
@@ -3,9 +3,12 @@ package com.prism.statistics.infrastructure.analysis.metadata.review.persistence
 import com.prism.statistics.domain.analysis.metadata.review.Review;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaReviewRepository extends ListCrudRepository<Review, Long> {
 
     Optional<Review> findByGithubReviewId(Long githubReviewId);
+
+    List<Review> findAllByPullRequestId(Long pullRequestId);
 }
