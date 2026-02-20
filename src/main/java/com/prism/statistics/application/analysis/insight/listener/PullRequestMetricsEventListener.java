6b85ec89f1feda6136e39f0af1a6601fbcc348e4
gyunnybot <gyunnybot4029@gmail.com>
2026-02-20 21:11:26 +0900

    refactor: PullRequestSavedEvent로 구독 변경

--- src/main/java/com/prism/statistics/application/analysis/insight/listener/PullRequestMetricsEventListener.java
+++ src/main/java/com/prism/statistics/application/analysis/insight/listener/PullRequestMetricsEventListener.java
@@ -1,7 +1,7 @@
 package com.prism.statistics.application.analysis.insight.listener;
 
 import com.prism.statistics.application.analysis.insight.PullRequestMetricsPublisher;
-import com.prism.statistics.application.analysis.metadata.pullrequest.event.PullRequestOpenCreatedEvent;
+import com.prism.statistics.application.analysis.metadata.pullrequest.event.PullRequestSavedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -14,7 +14,7 @@ public class PullRequestMetricsEventListener {
     private final PullRequestMetricsPublisher metricsPublisher;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handle(PullRequestOpenCreatedEvent event) {
-        metricsPublisher.publish(event);
+    public void handle(PullRequestSavedEvent event) {
+        metricsPublisher.publish(event.pullRequestId());
     }
 }
