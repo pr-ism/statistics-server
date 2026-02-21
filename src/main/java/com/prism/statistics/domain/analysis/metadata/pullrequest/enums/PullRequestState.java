ded86f04a480f1799c1b7bbe1c71872f1d639425
gyunnybot <gyunnybot4029@gmail.com>
2026-02-21 16:15:03 +0900

    feat: isClosureState 검증 메서드 추가

--- src/main/java/com/prism/statistics/domain/analysis/metadata/pullrequest/enums/PullRequestState.java
+++ src/main/java/com/prism/statistics/domain/analysis/metadata/pullrequest/enums/PullRequestState.java
@@ -42,4 +42,8 @@ public enum PullRequestState {
     public boolean isClosed() {
         return this == CLOSED;
     }
+
+    public boolean isClosureState() {
+        return this == MERGED || this == CLOSED;
+    }
 }
