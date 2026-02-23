7765cbe10e1315504536603d901ad24bf5a8d129
HyNS00 <hsk7953@gmail.com>
2026-02-23 17:41:42 +0900

    refactor: fileData 자료형 import 수정

--- src/main/java/com/prism/statistics/application/analysis/metadata/pullrequest/event/PullRequestSynchronizedEvent.java
+++ src/main/java/com/prism/statistics/application/analysis/metadata/pullrequest/event/PullRequestSynchronizedEvent.java
@@ -1,6 +1,6 @@
 package com.prism.statistics.application.analysis.metadata.pullrequest.event;
 
-import com.prism.statistics.application.analysis.metadata.pullrequest.dto.request.PullRequestOpenedRequest.FileData;
+import com.prism.statistics.application.analysis.metadata.pullrequest.dto.request.PullRequestSynchronizedRequest.FileData;
 import com.prism.statistics.application.analysis.metadata.pullrequest.event.PullRequestOpenCreatedEvent.CommitData;
 import com.prism.statistics.domain.analysis.metadata.pullrequest.vo.PullRequestChangeStats;
 
